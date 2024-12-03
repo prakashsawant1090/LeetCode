22e6cd6d1b090b95900eea520a767b4bef4de062
prakashsawant1090 <126107360+prakashsawant1090@users.noreply.github.com>
2024-12-03 12:14:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Cycle in a Directed Graph/cycle-in-a-directed-graph.java
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class DriverClass {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+            int V = sc.nextInt();
+            int E = sc.nextInt();
+            for (int i = 0; i < V; i++) list.add(i, new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                int u = sc.nextInt();
+                int v = sc.nextInt();
+                list.get(u).add(v);
+            }
+            if (new Solution().isCyclic(V, list) == true)
+                System.out.println("1");
+            else
+                System.out.println("0");
+
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/*Complete the function below*/
+
+class Solution {
+    // Function to detect cycle in a directed graph.
+    public boolean isCyclic(int V, ArrayList<ArrayList<Integer>> adj) {
+        ArrayList<Integer>topo=new ArrayList<>();
+     Queue<Integer>q=new LinkedList<>();
+     
+     int indegree[]=new int[V];
+     
+     for(int i=0;i<V;i++){
+         for(int it:adj.get(i)) indegree[it]++;
+     }
+         
+    for(int i=0;i<V;i++){
+        if(indegree[i]==0) q.offer(i);
+    }
+    
+    
+    while(!q.isEmpty()){
+        int curr=q.poll();
+        
+        topo.add(curr);
+        
+        for(int it:adj.get(curr)) {
+           indegree[it]--;
+           if(indegree[it]==0) q.offer(it);
+        }
+        
+        
+    }
+    
+    // System.out.print(topo);
+         
+      if(topo.size()!=V) return true;
+      return false;
+    }
+}
